6f6ca600b49491d8cd1c76233d3dae02e1ac35d0
safiat Qasim <safiatqasim17@gmail.com>
2023-08-29 22:37:56 +0500

    fundamental1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -8,3 +8,36 @@ let js = 'amazing';
       let firstName = "Safiat"
 
       console.log(firstName);
+
+let safiat_junaid = "SJ";
+let $function = 30;
+
+let person = "safiat";
+let PI = 3.145;
+
+let myFirstJob = "Writer";
+let myCurrentJob = "Developer";
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+/* Data Types */
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
