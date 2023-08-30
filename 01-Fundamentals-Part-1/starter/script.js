e54a2b2d2def10acef7acfb682a1a7da134b8d16
safiat Qasim <safiatqasim17@gmail.com>
2023-08-30 22:58:53 +0500

    fundamental1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 
       console.log(40+8+23-10);
 
@@ -21,14 +21,14 @@ let myCurrentJob = "Developer";
 console.log(myFirstJob);
 console.log(myCurrentJob);
 
-/* Data Types */
+// Data Types 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
 console.log(typeof true);
 console.log(typeof javascriptIsFun);
-console.log(typeof 23);
-console.log(typeof 'Jonas');
+console.log(typeof 30);
+console.log(typeof 'Safiat');
 
 javascriptIsFun = 'YES!';
 console.log(typeof javascriptIsFun);
@@ -41,3 +41,81 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+
+// Basic Operators
+// Math operators
+const now = 2037;
+const ageSafiat = now - 1994;
+const ageZunaira = now - 2011;
+console.log(ageSafiat, ageZunaira);
+
+console.log(ageSafiat * 2, ageSafiat / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstNam = 'Safiat';
+const lastName = 'Junaid';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageSafiat > ageZunaira); // >, <, >=, <=
+console.log(ageZunaira >= 18);
+
+const isFullAge = ageZunaira >= 18;
+
+console.log(now - 1994 > now - 2011);
+
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge); */
+
+// Coding Challenge #1
+
+/*Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall. */
+
+/*const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);*/
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
