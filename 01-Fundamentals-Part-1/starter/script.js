1aedd3c3065e35e06c548d849773a0306347cf3b
safiat Qasim <safiatqasim17@gmail.com>
2023-09-05 22:28:32 +0500

    fundamental1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -215,7 +215,58 @@ console.log('I am ' + 23 + ' years old');
 console.log('23' - '10' - 3);
 console.log('23' / '2');
 
-let n = '1' + 1; // '11'
+/*let n = '1' + 1; // '11'
 n = n - 1;
+console.log(n);*/
+
+/*let n = 2 + 3 + 4 + '5';
+console.log(n);*/
+
+let n = '10' - '4' - '3' - 2 + '5';
 console.log(n);
+
+// Truthy and Falsy Values
+
+// 5 falsy values: 0, '', undefined, null, NaN
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+/*const money = 0;
+if (money) {
+      console.log("Don't spend it all");
+} else {
+      console.log('You should get a job!');
+}*/
+
+/*const money = 100;
+if (money) {
+      console.log("Don't spend it all");
+} else {
+      console.log('You should get a job!');
+}*/
+
+/*let height;
+if (height) {
+      console.log('Yaya! height is defined');
+} else {
+      console.log('Height is undefined');
+}*/
+
+/*const height = 5.2;
+if (height) {
+      console.log('Yaya! height is defined');
+} else {
+      console.log('Height is undefined');
+}*/
+
+const height = 0;
+if (height) {
+      console.log('Yaya! height is defined');
+} else {
+      console.log('Height is undefined');
+}
+
 
