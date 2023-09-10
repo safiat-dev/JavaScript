00281e0d078329da2faaf4e02ddaad4967a66b1b
safiat Qasim <safiatqasim17@gmail.com>
2023-09-10 11:20:44 +0500

    fundamental2

--- Fundamentals-Part 2/script.js
+++ Fundamentals-Part 2/script.js
@@ -16,6 +16,7 @@ logger();
 logger();
 logger();
 
+// function declaration
 function fruitProcessor(apples, betroots) {
     console.log(apples, betroots);
     const juice = `Juice with ${apples} apples and ${betroots} betroots.`;
@@ -28,3 +29,21 @@ console.log(appleJuice);
 
 const appleBetrootJuice = fruitProcessor(4, 1);
 console.log(appleBetrootJuice);
+
+function calcAge1(birthYear) {
+    console.log(birthYear);
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1994);
+console.log(age1);
+
+// function expression
+
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1994);
+console.log(age1, age2);
+
