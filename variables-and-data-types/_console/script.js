25f038ad2b71d4200f3d4f17fa527751c59f3f37
safiat Qasim <safiatqasim17@gmail.com>
2023-09-14 14:04:51 +0500

    console

---
+++ variables-and-data-types/_console/script.js
@@ -0,0 +1,23 @@
+console.log(100); // number
+
+console.log('HI'); // string
+
+console.log(100, 'HI', false); // number, string, boolean
+
+const x = 100; // variable and value
+console.log(x); // calculate the value of x
+
+console.error('Alert');
+console.warn('Warning');
+
+console.table({name: 'Safiat', email: 'safiat@gmail.com'});
+
+console.group('Simple');
+console.error('Alert');
+console.warn('Warning');
+console.log(x);
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color:black; color: green;' // css styling
+console.log('%cHello Developers', styles);
+
